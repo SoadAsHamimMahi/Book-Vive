0a97ca82525acd1672d5ecefb1e58c730a9f571d
Soad As Hamim Mahi <soadashamimmahi@gmail.com>
2024-03-28 05:03:35 +0600

    Pricing Page added

--- src/components/Pricing/Pricing.jsx
+++ src/components/Pricing/Pricing.jsx
@@ -11,13 +11,13 @@ const Pricing = () => {
                 <h2 className="text-3xl font-bold text-black">Choose the right plan for you</h2>
             </div>
 
-            <div>
+            <div className='grid grid-cols-1 lg:grid-cols-3 gap-4 lg:w-5/6 mx-auto'>
                 <div className="card w-96 bg-base-100 shadow-xl">
-                    <div className="card-body">
+                    <div className="card-body hover:border-red-700 hover:border-2 hover:shadow-red-700 shadow-2xl transition duration-300">
                         <div className='bg-red-700 px-4 my-4 w-fit rounded-2xl text-white font-bold '>
-                            <p>ESSENTIAL PLAN</p>
+                            <p>BEST VALUE</p>
                         </div>
-                        <h2 className="card-title text-4xl font-bold">Books Free</h2>
+                        <h2 className="card-title text-4xl font-bold">Free Books</h2>
                         <p className='text-2xl font-semibold'>0$ <sub>/month</sub></p>
                         <hr className='my-3' />
 
@@ -26,7 +26,7 @@ const Pricing = () => {
                         </p>
 
                        <div className='text-xl flex flex-col gap-2 mb-4'>
-                       <p className='text-xl font-bold my-2'>Highlighted Features</p>
+                       <p className='text-xl font-bold my-2 text-red-700'>Highlighted Features</p>
                        <div className='flex items-center gap-2'>
                        <BsArrowRightSquareFill />
                         <p>Limited access</p>
@@ -69,7 +69,121 @@ const Pricing = () => {
                     </div>
                 </div>
 
-                
+                <div className="card w-96 bg-base-100 shadow-xl">
+                    <div className="card-body hover:border-green-700 hover:border-2 hover:shadow-green-700 shadow-2xl transition duration-300">
+                        <div className='bg-green-700 px-4 my-4 w-fit rounded-2xl text-white font-bold '>
+                            <p>ESSENTIAL PLAN</p>
+                        </div>
+                        <h2 className="card-title text-4xl font-bold">Pro Books</h2>
+                        <p className='text-2xl font-semibold'>29$ <sub>/month</sub></p>
+                        <hr className='my-3' />
+
+                        <p>
+                        Intermediate package for avid readers
+                        </p>
+
+                       <div className='text-xl flex flex-col gap-2 mb-4'>
+                       <p className='text-xl font-bold my-2 text-green-700'>Highlighted Features</p>
+                       <div className='flex items-center gap-2'>
+                       <BsArrowRightSquareFill />
+                        <p>Limited access</p>
+                       </div>
+
+                       <div className='flex items-center gap-2'>
+                       <BsArrowRightSquareFill />
+                        <p>Daily 10 Downloads</p>
+                       </div>
+
+                       <div className='flex items-center gap-2'>
+                       <BsArrowRightSquareFill />
+                        <p>No download restrictions</p>
+                       </div>
+                       <div className='flex items-center gap-2'>
+                       <BsArrowRightSquareFill />
+                        <p>Download for offline reading</p>
+                       </div>
+                       <div className='flex items-center gap-2'>
+                       <BsArrowRightSquareFill />
+                        <p>Ad-free experience</p>
+                       </div>
+                       <div className='flex items-center gap-2'>
+                       <BsArrowRightSquare />
+                        <p>Exclusive access to new releases</p>
+                       </div>
+                       <div className='flex items-center gap-2'>
+                       <BsArrowRightSquare />
+                        <p>Personalized recommendations</p>
+                       </div>
+                       <div className='flex items-center gap-2'>
+                       <BsArrowRightSquare />
+                        <p>Priority customer support</p>
+                       </div>
+                       </div>
+
+                        <div className="card-actions">
+                            <button className="btn btn-success text-white">Start a 14 Day Trial</button>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div className="card w-96 bg-base-100 shadow-xl">
+                    <div className="card-body hover:border-blue-700 hover:border-2 hover:shadow-blue-700 shadow-2xl transition duration-300">
+                        <div className='bg-blue-700 px-4 my-4 w-fit rounded-2xl text-white font-bold '>
+                            <p>ULTIMATE PLAN
+</p>
+                        </div>
+                        <h2 className="card-title text-4xl font-bold">Plus Books</h2>
+                        <p className='text-2xl font-semibold'>99$ <sub>/Year</sub></p>
+                        <hr className='my-3' />
+
+                        <p>
+                        Premium package for serious book lovers
+                        </p>
+
+                       <div className='text-xl flex flex-col gap-2 mb-4'>
+                       <p className='text-xl font-bold my-2 text-blue-700'>Highlighted Features</p>
+                       <div className='flex items-center gap-2'>
+                       <BsArrowRightSquareFill />
+                        <p>Limited access</p>
+                       </div>
+
+                       <div className='flex items-center gap-2'>
+                       <BsArrowRightSquareFill />
+                        <p>Daily 10 Downloads</p>
+                       </div>
+
+                       <div className='flex items-center gap-2'>
+                       <BsArrowRightSquareFill />
+                        <p>No download restrictions</p>
+                       </div>
+                       <div className='flex items-center gap-2'>
+                       <BsArrowRightSquareFill />
+                        <p>Download for offline reading</p>
+                       </div>
+                       <div className='flex items-center gap-2'>
+                       <BsArrowRightSquareFill />
+                        <p>Ad-free experience</p>
+                       </div>
+                       <div className='flex items-center gap-2'>
+                       <BsArrowRightSquareFill />
+                        <p>Exclusive access to new releases</p>
+                       </div>
+                       <div className='flex items-center gap-2'>
+                       <BsArrowRightSquareFill />
+                        <p>Personalized recommendations</p>
+                       </div>
+                       <div className='flex items-center gap-2'>
+                       <BsArrowRightSquareFill />
+                        <p>Priority customer support</p>
+                       </div>
+                       </div>
+
+                        <div className="card-actions">
+                            <button className="btn btn-success text-white">Start a 14 Day Trial</button>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
